9b94def5a2be0f5ad7ac340e7d6dbc04f1bb4273
mark-ruddy <1markruddy@gmail.com>
2022-04-05 18:29:20 +0100

    add general usage

---
+++ README.md
@@ -0,0 +1,55 @@
+# ipfetch
+CLI Tool for easy IP information fetching.  
+
+Credit to https://github.com/trakBan/ipfetch for the original idea.  
+
+This Rust implementation aims to provide stability and extra features, if you are interested in using the underlying Rust IP API library see https://github.com/mark-ruddy/seeip.
+
+# Installing
+### Cargo
+Install the binary using cargo:
+`cargo install ipfetch`
+
+### Compile
+Clone the source code from github and compile yourself:
+```
+git clone https://github.com/mark-ruddy/ipfetch
+cd ipfetch && cargo build --release
+sudo mv target/release/ipfetch /usr/local/bin
+```
+
+# Examples
+Run `ipfetch --help` to see available flags.  
+
+Get info on your own IP:
+```
+ipfetch --my-ip
+```
+
+Some systems can have both an IPv4 and an IPv6 assigned, to specifically check both:
+```
+ipfetch --my-ipv4 --my-ipv6
+```
+
+Get info on an IP:
+```
+ipfetch --ip 208.67.222.222
+```
+
+Get info on multiple IPs, IPv6 is supported:
+```
+ipfetch --ip 208.67.222.222 --ip 2620:0:ccc::2
+```
+
+Print IP info to terminal for the Google and Cloudflare DNS servers, and save this info to a file:
+```
+ipfetch --ip 8.8.8.8 --ip 1.1.1.1 --output dns-servers-info
+```
+
+Combine the flags to produce a report on your IPv6 and others in one command:
+```
+ipfetch --my-ipv6 --ip 8.8.8.8 -ip 208.67.222.222 --output ip-info-data
+```
+
+# Contributions
+Any and all contributions are appreciated - completely new features, bug fixes etc. Ensure your code is formatted with `rustfmt`.  
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,168 @@
 use clap::Parser;
-use seeip;
+use reqwest;
+use seeip::utils::GeoInfo;
+use std::fs;
+use std::io::Write;
+use tabled::builder::Builder;
+
+/// Print readable output of the IPs geographical info to the terminal including a country flag matching the IPs location
+fn print_geo_info(info: &GeoInfo, raw_github_url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let formatted_url = format!("{}/{}", raw_github_url, info.country.replace(" ", "_"));
+    let flag = reqwest::blocking::get(formatted_url)?.text()?;
+
+    let info_table = Builder::default()
+        .add_row(["IP", info.ip.as_str()])
+        .add_row(["Country", info.country.as_str()])
+        .add_row(["Organization", info.organization.as_str()])
+        .add_row(["Timezone", info.timezone.as_str()])
+        .add_row(["Continent Code", info.continent_code.as_str()])
+        .add_row(["Country Code", info.country_code.as_str()])
+        .add_row(["Country Code 3", info.country_code3.as_str()])
+        .add_row(["Region Code", info.region_code.as_str()])
+        .add_row(["Postal Code", info.postal_code.as_str()])
+        .add_row(["Area Code", info.area_code.to_string().as_str()])
+        .add_row(["DMA Code", info.dma_code.to_string().as_str()])
+        .add_row(["Offset", info.offset.to_string().as_str()])
+        .add_row(["ASN", info.asn.to_string().as_str()])
+        .add_row(["Longitude", info.longitude.to_string().as_str()])
+        .add_row(["Latitude", info.latitude.to_string().as_str()])
+        .build();
+
+    print!("{}", flag);
+    print!("{}", info_table);
+    Ok(())
+}
+
+/// Write code-friendly output of the IPs geographical info to a file
+fn write_geo_info(out: &String, info: &GeoInfo) -> Result<(), Box<dyn std::error::Error>> {
+    let data = format!(
+        "\
+        ip: {}
+        country: {}
+        organization: {}
+        timezone: {}
+        continent_code: {}
+        country_code: {}
+        country_code3: {}
+        region_code: {}
+        postal_code: {}
+        area_code: {}
+        dma_code: {}
+        offset: {}
+        asn: {}
+        longitude: {}
+        latitude: {}\n",
+        info.ip,
+        info.country,
+        info.organization,
+        info.timezone,
+        info.continent_code,
+        info.country_code,
+        info.country_code3,
+        info.region_code,
+        info.postal_code,
+        info.area_code,
+        info.dma_code,
+        info.offset,
+        info.asn,
+        info.longitude,
+        info.latitude
+    );
+    let mut out_file = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(out)?;
+    out_file.write_all(data.replace(" ", "").as_bytes())?;
+    Ok(())
+}
+
+/// Coordinate the writing the the file(if specified) and the printing to terminal
+/// Useful so that the --ip and --my-ip flags can all be handled identically by calling handle_info
+fn handle_info(info: &GeoInfo, output: &Option<String>) -> Result<(), Box<dyn std::error::Error>> {
+    match output {
+        Some(ref out) => write_geo_info(&out, &info)?,
+        None => (),
+    }
+    print_geo_info(
+        &info,
+        "https://raw.githubusercontent.com/mark-ruddy/ipfetch/main/country_flags",
+    )?;
+    Ok(())
+}
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// The IP used to fetch
-    #[clap(short, long, default_value = "")]
-    ip: String,
-    /// List of IPs to fetch
-    #[clap(long, default_value = "")]
-    ips: Vec<String>,
-}
+    /// An IP to fetch
+    #[clap(short, long)]
+    ip: Option<Vec<String>>,
+    /// Name of output file to store code-friendly fetched IP data
+    #[clap(short, long)]
+    output: Option<String>,
 
-fn handle_ips(ips: Vec<&str>) {
-    for ip in ips {
-        println!("in handle_ips: {}", ip);
-    }
+    /// Get info on your own IP
+    #[clap(long, takes_value = false)]
+    my_ip: bool,
+    /// Get info on your own IPv4
+    #[clap(long, takes_value = false)]
+    my_ipv4: bool,
+    /// Get info on your own IPv6
+    #[clap(long, takes_value = false)]
+    my_ipv6: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
-    // check if list of IPs is passed
-    handle_ips(ip_vec);
+    if args.my_ip {
+        match seeip::get_caller_geo() {
+            Ok(info) => match handle_info(&info, &args.output) {
+                Ok(_) => (),
+                Err(e) => eprintln!("Error handling caller IP info: {}", e),
+            },
+            Err(e) => eprintln!("Error making API call with caller's IP: {}", e),
+        }
+    }
 
-    // execution will reach here only if no list of IPs is passed
-    let ip = args.ip;
-    if ip == "" {}
-    println!("one IP: {}", ip);
+    if args.my_ipv4 {
+        match seeip::get_caller_geo_v4() {
+            Ok(info) => match handle_info(&info, &args.output) {
+                Ok(_) => (),
+                Err(e) => eprintln!("Error handling caller IPv4 info: {}", e),
+            },
+            Err(e) => eprintln!("Error making API call with caller's IPv4: {}", e),
+        }
+    }
+
+    if args.my_ipv6 {
+        match seeip::get_caller_geo_v6() {
+            Ok(info) => match handle_info(&info, &args.output) {
+                Ok(_) => (),
+                Err(e) => eprintln!("Error handling caller IPv6 info: {}", e),
+            },
+            Err(e) => eprintln!("Error making API call with caller's IPv6: {}", e),
+        }
+    }
+
+    // Iterate through each passed --ip flag now
+    match args.ip {
+        Some(ips) => {
+            for ip in ips {
+                match seeip::get_geo(ip.as_str()) {
+                    Ok(info) => match handle_info(&info, &args.output) {
+                        Ok(_) => (),
+                        Err(e) => eprintln!("Error handling IP info: {}", e),
+                    },
+                    Err(e) => eprintln!("Error making API call with IP {}: {}", ip, e),
+                }
+            }
+        }
+        None => (),
+    }
+
+    match args.output {
+        Some(out) => println!("\nOutput written to file: {}", out),
+        None => (),
+    }
 }
